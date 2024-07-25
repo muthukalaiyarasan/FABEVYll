6652464cf4f604701cfb463c7ee237f0e7d5c6ec
muthukalaiyarasan <132567401+muthukalaiyarasan@users.noreply.github.com>
2024-07-25 16:11:30 +0530

    Add files via upload

---
+++ dom color/js/main.js
@@ -0,0 +1,31 @@
+var section=document.getElementById("section")
+
+var button=document.createElement("button")
+section.append(button)
+button.innerHTML="clickme"
+button.setAttribute("onclick","show()")
+
+let color=["yellow","green","blue","white","red"]
+
+function show(){
+	let name=color.map((e)=>{
+		let b=array.length
+		let a=Mathfloor(Math.random()*b)
+		let c=console.log(color[a])
+	})
+	if(a==0){
+	let black = body.setAttribute("class","bg-danger")
+}
+else if(a==1){
+	let black = body.setAttribute("class","bg-warning")
+}
+else if(a==2){
+	let black = body.setAttribute("class","bg-primary")
+}
+else{
+	let black = body.setAttribute("class","bg-success")
+}
+
+
+	 
+}
